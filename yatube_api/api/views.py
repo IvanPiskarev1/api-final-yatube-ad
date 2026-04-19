0bf9863e559e9aa709944ec0a9144dcbcf5e6ee0
IvanPiskarev1 <ivan.piskarev50@mail.ru>
2026-04-19 22:58:20 +0300

    Add 6 commit

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -12,7 +12,8 @@ from .serializers import (CommentSerializer, FollowSerializer,
 
 class PostPagination(LimitOffsetPagination):
     def paginate_queryset(self, queryset, request, view=None):
-        if 'limit' not in request.query_params and 'offset' not in request.query_params:
+        if ('limit' not in request.query_params
+                and 'offset' not in request.query_params):
             return None
         return super().paginate_queryset(queryset, request, view)
 
